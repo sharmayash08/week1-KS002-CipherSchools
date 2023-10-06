aac0fcef2b68abcbb2a50bf02315769491d816ec
Yash Sharma <yash280876@gmail.com>
2023-10-06 22:11:52 +0530

    Added Lecture 7

---
+++ Lecture 7.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	/*int password;
+	
+	cout<<"Enter the password:"<<endl;
+	cin>>password;
+	
+	while(password<999999){
+		cout<<"The password does not meet the required conditions, please enter the password again";
+		cin>>password;
+	}
+	cout<<"The user entered the correct password"<<endl;*/
+
+    /*int password;
+	
+	do{
+		cin>>password;
+	}while(password<999999);*/
+
+    int password;
+	int count=0;
+//	while(1){
+//		cout<<"choclates ";
+//		count++;
+//		if(count == 100){
+//			break;
+//		}
+//	}
+
+    for(int i=0; ;i++){
+//    	cout<<"pranav"<<" ";
+        cout<<i<<" ";
+        if(i>100) break;
+	}
+
+
+	return 0;
+}
